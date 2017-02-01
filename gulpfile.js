ae7479f4a7dbbdc609343d34a16ccb6252fbbf75
Joshua Azemoh <joshazemoh@gmail.com>
2017-02-01 15:26:21 +0100

    refactor gulpfile to use ES6

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
-var exec = require('child_process').exec;
-var gulp = require('gulp');
-var browserSync = require('browser-sync');
-var sass = require('gulp-sass');
-var autoprefix = require('gulp-autoprefixer');
-var uglify = require('gulp-uglifyjs');
+const exec = require('child_process').exec;
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass');
+const autoprefix = require('gulp-autoprefixer');
+const uglify = require('gulp-uglifyjs');
 
-var config = {
+const config = {
   publicDir: '_site/',
   cssDir: 'assets/css/',
   jsDir: 'assets/js/',
@@ -16,89 +16,67 @@ var config = {
  * Build the Jekyll Site
  * for development
  */
-gulp.task('jekyll:devbuild', function (done) {
+gulp.task('jekyll:devbuild', (done)  => {
   browserSync.notify(config.jekyllBuildMessage);
-  return exec('bundle exec jekyll build --config "_config.yml,_config.dev.yml"', {
-      stdio: 'inherit'
-    })
+  return exec('bundle exec jekyll build --config "_config.yml,_config.dev.yml"', { stdio: 'inherit' })
     .on('close', done);
 });
-
 
 /**
  * Build the Jekyll Site
  * for production
  */
-gulp.task('jekyll:build', function (done) {
-  return exec('bundle exec jekyll build', {
-      stdio: 'inherit'
-    })
+gulp.task('jekyll:build', (done) => {
+  return exec('bundle exec jekyll build', { stdio: 'inherit' })
     .on('close', done);
 });
 
 /**
  * Rebuild Jekyll and reload page
  */
-gulp.task('jekyll:rebuild', ['jekyll:devbuild'], function () {
-  browserSync.reload();
-});
-
+gulp.task('jekyll:rebuild', ['jekyll:devbuild'], browserSync.reload);
 
 /**
  * Wait for jekyll:devbuild, then launch the Server
  */
-gulp.task('browser-sync', ['sass', 'jekyll:devbuild', 'minify:js'], function () {
+gulp.task('browser-sync', ['sass', 'jekyll:devbuild', 'minify:js'], () => {
   browserSync({
-    server: {
-      baseDir: config.publicDir
-    },
+    server: { baseDir: config.publicDir },
     notify: false
   });
 });
 
-
 /**
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
-gulp.task('sass', function () {
-  return gulp
-    .src(config.cssDir + '*.scss')
-    .pipe(sass({
-        includePaths: [config.cssDir], outputStyle: 'compressed'
-      }))
+gulp.task('sass', () =>
+  gulp
+    .src(`${config.cssDir}*.scss`)
+    .pipe(sass({ includePaths: [config.cssDir], outputStyle: 'compressed' }))
     .on('error', sass.logError)
-    .pipe(autoprefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
-        cascade: true
-      }))
-    .pipe(gulp.dest(config.publicDir + config.cssDir))
-    .pipe(browserSync.reload({
-        stream: true
-      }))
-    .pipe(gulp.dest(config.cssDir));
-});
+    .pipe(autoprefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+    .pipe(gulp.dest(`${config.publicDir}${config.cssDir}`))
+    .pipe(browserSync.reload({ stream: true }))
+    .pipe(gulp.dest(config.cssDir)));
 
 /**
  * minify javaScript files
  */
-gulp.task('minify:js', function () {
-  return gulp.src(config.jsDir + 'script.js')
-    .pipe(uglify('script.min.js', {
-        outSourceMap: true
-      }))
-    .pipe(gulp.dest(config.publicDir + config.jsDir))
-    .pipe(browserSync.reload({
-        stream: true
-      }))
-    .pipe(gulp.dest(config.jsDir));
-});
+gulp.task('minify:js', () =>
+  gulp
+    .src(`${config.jsDir}script.js`)
+    .pipe(uglify('script.min.js', { outSourceMap: true }))
+    .pipe(gulp.dest(`${config.publicDir}${config.jsDir}`))
+    .pipe(browserSync.reload({ stream: true }))
+    .pipe(gulp.dest(config.jsDir)));
 
 /**
  * Watch scss files for changes & recompile
  * Watch html/md files, run jekyll & reload BrowserSync
  */
-gulp.task('watch', function () {
-  gulp.watch(config.cssDir + '**/*.scss', ['sass']);
-  gulp.watch(config.jsDir + 'script.js', ['minify:js']);
+gulp.task('watch', () => {
+gulp.watch(`${config.cssDir}**/*.scss`, ['sass']);
+gulp.watch(`${config.jsDir}script.js`, ['minify:js']);
   gulp.watch([
 		'*.html',
 		'_layouts/*.html',
