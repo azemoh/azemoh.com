2fa0eae950f570c34c6a40cd684d6570a2f5989b
Joshua Azemoh <joshazemoh@gmail.com>
2022-08-15 23:23:11 -0400

    Update build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 const exec = require('child_process').exec;
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
-const sass = require('gulp-sass');
+const nodeSass = require('node-sass');
+const sass = require('gulp-sass')(nodeSass);
 const autoprefix = require('gulp-autoprefixer');
 const uglify = require('gulp-uglifyjs');
 
@@ -12,89 +13,73 @@ const config = {
   jekyllBuildMessage: '<span style="color: grey">Running:</span> $ jekyll build'
 };
 
-/**
- * Build the Jekyll Site
- * for development
- */
-gulp.task('jekyll:devbuild', (done)  => {
+// Build the Jekyll Site for development
+function devBuild(done) {
   browserSync.notify(config.jekyllBuildMessage);
   return exec('bundle exec jekyll build --config "_config.yml,_config.dev.yml"', { stdio: 'inherit' })
     .on('close', done);
-});
+}
 
-/**
- * Build the Jekyll Site
- * for production
- */
-gulp.task('jekyll:build', (done) => {
+
+// Build the Jekyll Site for production
+function prodBuild(done) {
   return exec('bundle exec jekyll build', { stdio: 'inherit' })
     .on('close', done);
-});
+}
 
-/**
- * Rebuild Jekyll and reload page
- */
-gulp.task('jekyll:rebuild', ['jekyll:devbuild'], browserSync.reload);
+// Rebuild Jekyll and reload page
+const rebuild = gulp.series(devBuild, browserSync.reload);
 
-/**
- * Wait for jekyll:devbuild, then launch the Server
- */
-gulp.task('browser-sync', ['sass', 'jekyll:devbuild', 'minify:js'], () => {
-  browserSync({
-    server: { baseDir: config.publicDir },
-    notify: false
-  });
-});
+// minify javaScript files
+function minifyJs() {
+  return gulp.src(`${config.jsDir}script.js`)
+    .pipe(uglify('script.min.js', { outSourceMap: true }))
+    .pipe(gulp.dest(`${config.publicDir}${config.jsDir}`))
+    .pipe(browserSync.reload({ stream: true }))
+    .pipe(gulp.dest(config.jsDir));
+}
 
-/**
- * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
- */
-gulp.task('sass', () =>
-  gulp
-    .src(`${config.cssDir}*.scss`)
-    .pipe(sass({ includePaths: [config.cssDir], outputStyle: 'compressed' }))
+function sassBuild() {
+  return gulp.src(`${config.cssDir}*.scss`)
+    .pipe(sass({ includePaths: [config.cssDir].concat(require('bourbon').includePaths), outputStyle: 'compressed' }))
     .on('error', sass.logError)
     .pipe(autoprefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
     .pipe(gulp.dest(`${config.publicDir}${config.cssDir}`))
     .pipe(browserSync.reload({ stream: true }))
-    .pipe(gulp.dest(config.cssDir)));
+    .pipe(gulp.dest(config.cssDir));
+}
 
-/**
- * minify javaScript files
- */
-gulp.task('minify:js', () =>
-  gulp
-    .src(`${config.jsDir}script.js`)
-    .pipe(uglify('script.min.js', { outSourceMap: true }))
-    .pipe(gulp.dest(`${config.publicDir}${config.jsDir}`))
-    .pipe(browserSync.reload({ stream: true }))
-    .pipe(gulp.dest(config.jsDir)));
+// Wait for jekyll-build, then launch the server
+const syncBrowser = gulp.series(sassBuild, devBuild, minifyJs, (done) => {
+  browserSync({
+    server: { baseDir: config.publicDir },
+    notify: false
+  });
 
-/**
- * Watch scss files for changes & recompile
- * Watch html/md files, run jekyll & reload BrowserSync
- */
-gulp.task('watch', () => {
-gulp.watch(`${config.cssDir}**/*.scss`, ['sass']);
-gulp.watch(`${config.jsDir}script.js`, ['minify:js']);
-  gulp.watch([
-		'*.html',
-		'_layouts/*.html',
-		'writing/*.html',
-		'_includes/**/*.html',
-		'_posts/*.md',
-		'assets/img/**/*',
-		'*.yml'
-	], ['jekyll:rebuild']);
+  done();
 });
 
-/**
- * Build site without starting server and watching for changes.
- */
-gulp.task('build', ['sass', 'jekyll:build', 'minify:js']);
+// Watch scss files for changes & recompile
+// Watch html/md files, run jekyll & reload BrowserSync
+function watch(done) {
+  gulp.watch(`${config.cssDir}*.scss`, sassBuild);
+  gulp.watch(`${config.jsDir}script.js`, minifyJs);
+  gulp.watch([
+    '*.html',
+    '_layouts/*.html',
+    'writing/*.html',
+    '_includes/**/*.html',
+    '_posts/*.md',
+    'assets/img/**/*',
+    '*.yml'
+  ], rebuild);
+}
 
-/**
- * Default task, running just `gulp` will compile the sass,
- * compile the jekyll site, launch BrowserSync & watch files.
- */
-gulp.task('default', ['browser-sync', 'watch']);
+// Build site without starting server or watching files
+const build = gulp.series(sassBuild, prodBuild, minifyJs);
+
+module.exports = {
+  build,
+  devBuild,
+  default: gulp.series(syncBrowser, watch),
+};
